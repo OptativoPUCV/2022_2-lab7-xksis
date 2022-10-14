0f1c873e96cc81fa21e0ada2c19cd2eb484dcc69
Replit user <>
2022-10-14 23:00:57 +0000

    new try

--- heap.c
+++ heap.c
@@ -49,7 +49,7 @@ void heap_pop(Heap* pq){
   pq->heapArray[0] = pq->heapArray[pq->size];
   heapElem* aux = (heapElem*)malloc(sizeof(heapElem));
   aux = pq->heapArray[1];
-  Heap* prioridad= pq->heapArray[0].priority;
+  int prioridad= pq->heapArray[0].priority;
 
   while(aux <= pq->size && pq->heapArray[aux].priority > prioridad || aux+1 <= pq->size && pq->heapArray[aux+1].priority > prioridad)
   {
