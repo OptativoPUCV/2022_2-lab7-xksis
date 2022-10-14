8a5e556578accea47ae0f0a930b4eeeb9635bdca
Replit user <>
2022-10-14 22:56:16 +0000

    new try

--- heap.c
+++ heap.c
@@ -47,8 +47,8 @@ void heap_push(Heap* pq, void* data, int priority){
 void heap_pop(Heap* pq){
   pq->size--;
   pq->heapArray[0] = pq->heapArray[pq->size];
-  aux = pq->heapArray[1];
-  priority = pq->heapArray[0].priority;
+  Heap *aux = pq->heapArray[1];
+  Heap *priority = pq->heapArray[0].priority;
 
   while(aux <= pq->size && pq->heapArray[aux].priority > priority || aux+1 <= pq->size && pq->heapArray[aux+1].priority > priority)
   {
@@ -57,7 +57,6 @@ void heap_pop(Heap* pq){
     pq->heapArray[(aux-1)/2] = pq->heapArray[aux];
     pq->heapArray[aux] = temp;
     aux = aux*2+1;
-    
     
   }
   
